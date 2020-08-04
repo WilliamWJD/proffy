c083d35f0731fb933a53022c88e3b8546bbf90f2
williamwjd <william.jose.wjd@gmail.com>
2020-08-04 11:23:38 -0300

    cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const server = express();
 
 const port = 3333;
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
