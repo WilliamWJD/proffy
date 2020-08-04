aa610ecebfa5c73c252c8114a20931d37d711b27
williamwjd <william.jose.wjd@gmail.com>
2020-08-04 11:21:55 -0300

    listando total de conexões

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = Router();
 
 routes.post('/classes', ClassesController.store);
 routes.get('/classes', ClassesController.index);
+
+routes.post('/connections', ConnectionsController.store);
+routes.get('/connections', ConnectionsController.index);
 
 export default routes;
