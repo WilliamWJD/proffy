87339837906a1d5275436e7e4886832edb6a40b1
williamwjd <william.jose.wjd@gmail.com>
2020-08-06 11:52:56 -0300

    configurando tab navigation

--- mobile/src/routes/AppStack.tsx
+++ mobile/src/routes/AppStack.tsx
@@ -6,6 +6,7 @@ const { Navigator, Screen } = createStackNavigator();
 
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
+import StudyTabs from './StudyTabs';
 
 function AppStack() {
   return (
@@ -13,6 +14,8 @@ function AppStack() {
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
+
+        <Screen name="Study" component={StudyTabs} />
       </Navigator>
     </NavigationContainer>
   );
