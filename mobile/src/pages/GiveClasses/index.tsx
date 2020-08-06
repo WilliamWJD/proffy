673a10dfcab8bb59bd1864c907a2925d2be753b4
williamwjd <william.jose.wjd@gmail.com>
2020-08-06 11:24:51 -0300

    criando tela GiveClasses

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, ImageBackground } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import givClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
 
 function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
   return (
-    <View>
-      <Text>Give Classes</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={givClassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor em nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton style={styles.okButton} onPress={handleNavigateBack}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
     </View>
   );
 }
