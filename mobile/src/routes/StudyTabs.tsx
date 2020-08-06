e1b81728729367fc5b36ef7b75a30c738774b625
williamwjd <william.jose.wjd@gmail.com>
2020-08-06 12:11:09 -0300

    ajustando cor do icone da tab

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -42,8 +42,14 @@ function StudyTabs() {
         component={TeacherList}
         options={{
           tabBarLabel: 'Proffys',
-          tabBarIcon: ({ color, size, focus }) => {
-            return <Ionicons name="ios-easel" size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-easel"
+                size={size}
+                color={focused ? '#8257e5' : color}
+              />
+            );
           },
         }}
       />
@@ -52,8 +58,14 @@ function StudyTabs() {
         component={Favorites}
         options={{
           tabBarLabel: 'Favoritos',
-          tabBarIcon: ({ color, size, focus }) => {
-            return <Ionicons name="ios-heart" size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-heart"
+                size={size}
+                color={focused ? '#8257e5' : color}
+              />
+            );
           },
         }}
       />
