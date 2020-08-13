02f35c293a4e019f29803a64896f243fcbb63615
williamwjd <william.jose.wjd@gmail.com>
2020-08-13 16:01:43 -0300

    ajustando tabela de login

--- backend/src/controllers/ForgotPasswordController.ts
+++ backend/src/controllers/ForgotPasswordController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
+import crypto from 'crypto';
+
 import db from '../database/connection';
 import Mail from '../lib/mail';
-import { hashGenerator } from '../utils/hashBcrypts';
 
 class ForgotPasswordController {
   async store(req: Request, res: Response) {
@@ -12,20 +13,31 @@ class ForgotPasswordController {
       email,
     });
 
-    const { name } = checkUser[0];
+    const { id, name } = checkUser[0];
 
     if (!checkUser[0]) {
       return res.status(401).json({ error: 'User not found' });
     }
 
+    const token = crypto.randomBytes(20).toString('hex');
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
+
+    await db('logins').where('id', '=', id).update({
+      passwordRestToken: token,
+      passwordResetExpires: now,
+    });
+
     await Mail.sendMail({
       to: email,
       subject: 'E-mail de recuperação de senha',
-      text: `Olá ${name} essa é sua senha provisória, não se esqueça de estar alterando ela em configurações de perfil.`,
+      text: `Olá ${name} você esqueceu sua senha ? não se preocupe, utilize esse token: ${token} para estar alterando sua senha.`,
     });
 
-    return res.json({});
+    return res.send();
   }
+
+  async resetPassword(req: Request, res: Response) {}
 }
 
 export default new ForgotPasswordController();
